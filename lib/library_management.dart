60389839a7cbeee49dfa9fb2f27b750eb1259235
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2022-12-06 17:59:23 +0600

    Login page design & Login with Rest API

---
+++ lib/library_management.dart
@@ -0,0 +1,179 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class LibraryManagement extends StatefulWidget {
+  const LibraryManagement({Key? key}) : super(key: key);
+
+  @override
+  State<LibraryManagement> createState() => _LibraryManagementState();
+}
+
+class _LibraryManagementState extends State<LibraryManagement> {
+
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Library Management",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontWeight: FontWeight.w200),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ListView(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 100.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.library_books,size: 30,color: Colors.brown,),
+                        Text("University of Dhaka",
+                          style: TextStyle(fontWeight: FontWeight.w200,color: Colors.blue,fontSize: 30),)
+                      ],
+                    ),
+                  ),
+                  usernametextFieldView(),
+                  passwordtextFieldView(),
+                  loginButton()
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  //Username Field
+
+ Widget usernametextFieldView(){
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.all(20.0),
+      child: TextFormField(
+        controller: usernameController,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(8.0),
+          hintText: "Enter User Name",
+          hintStyle: TextStyle(color: Colors.grey),
+          prefixIcon: Icon(Icons.person,color: Colors.grey,),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: BorderSide(color: Colors.white12),
+          )
+        ),
+      ),
+    );
+ }
+
+ //Password Field
+
+ Widget passwordtextFieldView(){
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      child: TextFormField(
+        obscureText: true,
+        controller: passwordController,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(8.0),
+          hintText: "Enter your password",
+          hintStyle: TextStyle(color: Colors.grey),
+          prefixIcon: Icon(Icons.lock,color: Colors.grey,),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: BorderSide(color: Colors.white12),
+
+          )
+        ),
+      ),
+    );
+ }
+//Login Button
+ Widget loginButton(){
+   return Column(
+     children: [
+       GestureDetector(
+         onTap: (){
+           login(usernameController.text.toString(),passwordController.text.toString(),context);
+
+         },
+         child: Container(
+          // padding: EdgeInsets.all(20),
+           height: MediaQuery.of(this.context).size.height/15,
+           width: MediaQuery.of(this.context).size.width/4,
+           decoration: BoxDecoration(
+             color: Colors.green,
+             borderRadius: BorderRadius.circular(10),
+             border: Border.all(color: Colors.black26),
+             boxShadow:[BoxShadow(color: Colors.grey.withOpacity(0.3),
+                 spreadRadius: 5,
+                 blurRadius: 7,
+                 offset: Offset(0, 3)) ]
+
+           ),
+           child: Center(child: Text("Login",style:
+           TextStyle(color: Colors.white,
+               fontWeight: FontWeight.w400,
+               fontSize: 18),)),
+
+         ),
+         ),
+     ],
+   );
+ }
+}
+//Login Function, call from API.
+void login(String username, String password,BuildContext context) async {
+  try{
+    Response response = await post(Uri.parse('https://reqres.in/api/register'),
+        body:{
+              'email': username,
+              'password' : password
+    });
+    if(response.statusCode == 200){
+      var data = jsonDecode(response.body.toString());
+
+      print(data['token']);
+      print("Account login successfully");
+      Navigator.push(context, MaterialPageRoute(builder: (context) =>UserDetails()));
+      
+    }
+    else{
+      print('Login Failed');
+    }
+  }
+  catch(e) {
+    print(e.toString());
+  }
+}
+
+//User Details Route
+class UserDetails extends StatefulWidget {
+  const UserDetails({Key? key}) : super(key: key);
+
+  @override
+  State<UserDetails> createState() => _UserDetailsState();
+}
+
+class _UserDetailsState extends State<UserDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("User Details"),
+      ),
+    );
+  }
+}
+
+
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:librarymanagment/library_management.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: LibraryManagement()
+  ));
+}
+
